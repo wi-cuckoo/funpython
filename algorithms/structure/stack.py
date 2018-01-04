e93457c100acdd3d068014dd9fd7b54b3bdb52be
Louis Wei <wi@localhost.localdomain>
2018-01-04 21:35:35 +0800

    add cacl example

--- algorithms/structure/stack.py
+++ algorithms/structure/stack.py
@@ -50,6 +50,55 @@ def base_convert(num, base):
 
     return res
 
+def infix2sufix(exp):
+    ops = {'*': 3, '/': 3, '+': 1, '-': 1, '(': 0}
+    suffix_exp = ''
+    stack = Stack()
+    for c in exp.split():
+        if c in '0123456789':
+            suffix_exp += c
+        elif c == '(':
+            stack.push(c)
+        elif c == ')':
+            op = stack.pop()
+            while op != '(':
+                suffix_exp += op
+                op = stack.pop()
+        else:
+            while (not stack.is_empty()) and \
+                (ops[stack.peek()] > ops[c]):
+                suffix_exp += stack.pop()
+            stack.push(c)
+
+    while not stack.is_empty():
+        suffix_exp += stack.pop()
+
+    return suffix_exp
+
+def cacl(sufix):
+    def do_cal(num1, num2, op):
+        if op == '*':
+            return num1 * num2
+        if op == '/':
+            return num1 / num2
+        if op == '+':
+            return num1 + num2
+        return num1 - num2
+
+    stack = Stack()
+    for c in sufix:
+        if c in '0123456789':
+            stack.push(int(c))
+            continue
+        num1 = stack.pop()
+        num2 = stack.pop()
+        stack.push(do_cal(num1, num2, c))
+    return stack.pop()
+
+
+
 if __name__ == '__main__':
     res = base_convert(255, 2)
-    print(res)
+    # print(res)
+    sufix = infix2sufix('( 1 + 2 ) * ( 3 + 4 + 3 )')
+    print(cacl(sufix))
