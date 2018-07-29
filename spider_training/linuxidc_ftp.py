7d7339d591c09ab132d59a8f97462d83390748ba
wi-cuckoo <yp_liuwei@163.com>
2018-07-29 23:39:03 +0800

    update linuxidc

--- spider_training/linuxidc_ftp.py
+++ spider_training/linuxidc_ftp.py
@@ -1,15 +1,26 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
 import requests
+import re
 from bs4 import BeautifulSoup
 
 ROOT_URL = 'http://linux.linuxidc.com/'
 
 
 class LinuxIDCCrawl:
-    pass
+
+	def __init__(self):
+		self.session = requests.Session()
+
+	def run(self):
+		r = self.session.get(ROOT_URL)
+		if not r.ok:
+			return
+
+		soup = BeautifulSoup(r.content, "html.parser")
+		print soup.find_all('a', href=re.compile('folder='))
 
 
 if __name__ == '__main__':
-    pass
+    LinuxIDCCrawl().run()
